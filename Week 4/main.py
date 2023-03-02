10c73d63bf0afb1a8ef1bb3d883187a9310e41d2
anamitra137 <anamitra137@gmail.com>
2023-03-03 02:06:57 +0530

    Now able to fetch tables from the db

--- Week 4/main.py
+++ Week 4/main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 import psycopg2
 import secrets
+import pandas as pd
 
 
 app = Flask(__name__)
@@ -20,14 +21,33 @@ def get_db_conn():
 def index():
     if not session.get('logged_in'):
         return redirect('/login')
-    return render_template('index.html')
+    
+    connection=get_db_conn()
+    cursor=connection.cursor()
+
+    cursor.execute("SELECT * FROM patient")
+    columns = [desc[0] for desc in cursor.description]
+    records=cursor.fetchall()
+    df_patient = pd.DataFrame(records, columns=columns)
+
+    cursor.execute("SELECT * FROM appointment")
+    columns = [desc[0] for desc in cursor.description]
+    records=cursor.fetchall()
+    df_appointment = pd.DataFrame(records, columns=columns)
+
+    cursor.close()
+    connection.close()
+
+    return render_template('index.html', 
+                           patient_table=[df_patient.to_html(classes='table table-striped table-sm', header=True, index=False, border=0, justify='left')],
+                           appointment_table=[df_appointment.to_html(classes='table table-striped table-sm', header=True, index=False, border=0, justify='left')])
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        print(username, password)
+
         # connection = get_db_conn()
         # cursor = connection.cursor()
         # cursor.execute("SELECT * FROM users WHERE username = %s AND password = %s", (username, password))
